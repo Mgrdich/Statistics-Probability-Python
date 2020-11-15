94e8dc78d75c82d3f2cd256ffa147207298d325e
Mgo <mgotm13@gmail.com>
2020-11-16 01:12:11 +0400

    Monty Hall Problem

--- monty_hall.py
+++ monty_hall.py
@@ -4,26 +4,40 @@ from random import randint
 
 
 class MontyHALL:
-    monty_doors = []
-    player_chosen_door = None
-    car_door_index = None
+    # constant through whole object cycle
     number_doors = 0
     success = 0
     experiment_number = 0
 
+    # changed with every go
+    monty_doors = []  # TODO change it to a number or dictionary format
+    player_chosen_door = None
+    car_door_index = None
+
     def __init__(self, number_doors):
         self.number_doors = number_doors
 
-    def start(self, switch, times=1):
+    def re_initialize(self):
+        self.monty_doors = []
+        self.player_chosen_door = None
+        self.car_door_index = None
+
+    def start(self, switch, times=1, single_experiment_detail=False):
         # start the program here
         self.experiment_number = times
+        experiment_times_local = times
 
-        self.generate_monty_cases()
-        self.player_chose()
-        self.monty_reveals_details_switch(switch)
+        while experiment_times_local:
+            self.re_initialize()
+            self.generate_monty_cases()
+            self.player_chose()
+            self.monty_reveals_details_switch(switch)
+            experiment_times_local = experiment_times_local - 1
+            if single_experiment_detail:
+                self.print_experiment_details()
 
         # end of the experiment reveal data
-        self.print_experiment_details()
+        self.print_results(switch)
 
     def player_chose(self):
         self.player_chosen_door = randint(0, self.number_doors - 1)
@@ -46,9 +60,19 @@ class MontyHALL:
                 self.success = self.success + 1
 
     def print_experiment_details(self):
+        # singular run detail
         self.print_monty_doors()
         self.print_player_chosen_door()
         self.print_car_position()
+
+    def print_results(self, switch):
+        print("/**************************************************************/")
+        print("Strategy Switch: ", switch)
+        print("Success: ", self.success)
+        print("Losses: ", self.experiment_number - self.success)
+        print("Total: ", self.experiment_number)
+        print("Ratio: ", self.success / self.experiment_number)
+        print("/**************************************************************/")
 
     def print_monty_doors(self):
         print("Monty Result Array:", self.monty_doors)
@@ -61,6 +85,4 @@ class MontyHALL:
 
 
 ob = MontyHALL(3)
-ob.start(True)
-print(ob.success)
-print(ob.experiment_number)
+ob.start(True, 999999999999999999, False)
