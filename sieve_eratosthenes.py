7479e2d29d34007eacebd224b62871a95bf0e167
Mgo <mgotm13@gmail.com>
2020-11-24 23:04:52 +0400

    Sieve of Erotosthenes initial

---
+++ sieve_eratosthenes.py
@@ -0,0 +1,12 @@
+class SIEVE_ERATOSTHENES:
+    range = 0
+    primes_array = []
+
+    def __init__(self, number_range):
+        self.range = number_range
+
+    def run(self):
+        print(self)
+
+    def print_results(self):
+        print(self)
