46f164d26cb25678283fc5e7b6ca22f50e597df3
Mgo <mgotm13@gmail.com>
2020-11-25 00:12:57 +0400

    Sieve of Erotosthenes initial

--- sieve_eratosthenes.py
+++ sieve_eratosthenes.py
@@ -1,5 +1,33 @@
-def SieveEratosthenes(number_range):
-    print(number_range)
+class SIEVE_ERATOSTHENES:
+    range = 0
+    primes_array = []  # True will be indexes that are prime
+    __p = 2  # prime Index
+
+    def __init__(self, number_range, split_print=None):
+        self.range = [True in range(number_range + 1)]
+        self.range = number_range
+        print(range)
+        self.range[0] = False  # 0 is not a prime
+        self.range[1] = False  # 1 is not a prime
+        if split_print:
+            self.print_stop = split_print
+
+    def run(self):
+        while self.__p * self.__p <= self.range:
+            # if number index is not changed then it is prime
+            if self.primes_array[self.__p]:
+
+                # Update all the prime multiplies if 2 then 4 , 6, 8, ....
+                for i in range(self.__p * 2, self.range + 1, self.__p):
+                    self.primes_array[i] = False
+
+                self.__p += 1
+
+                # Print all prime numbers
+                for p in range(self.range + 1):
+                    if self.primes_array[p]:
+                        print(p)
 
 
-SieveEratosthenes(500)
+ob = SIEVE_ERATOSTHENES(100)
+ob.run()
