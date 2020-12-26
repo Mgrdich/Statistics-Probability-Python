efae8e6210ed003f8ffe07c589219c0e8b6ae66f
Mgo <mgotm13@gmail.com>
2020-12-26 18:52:32 +0400

    Binomial Distribution Initial

--- binomial_distribution.py
+++ binomial_distribution.py
@@ -1,2 +1,4 @@
-def binomial_distribution():
+# binomial distribution with parameter (number_trials, probability)
+# trial tested size times
+def binomial_distribution(number_trials, probability, size=1):
     return
