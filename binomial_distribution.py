bb56c0845264e6289ffc321f0353c8f057cab8f8
Mgo <mgotm13@gmail.com>
2020-12-27 15:36:07 +0400

    Binomial Distribution

--- binomial_distribution.py
+++ binomial_distribution.py
@@ -4,8 +4,12 @@ from functions.math import combination
 
 # binomial distribution with parameter (number_trials, probability)
 # trial tested size times
-def binomial_distribution(number_trials: int, probability: float, size: int = 1):
-    return
+def binomial_distribution(number_trials: int, probability: float, tested: int = 1):
+    success = 1
+    combinations = combination(number_trials, success)
+    prob_win = pow(probability, success)
+    prob_lose = pow((1 - probability), (number_trials - success))
+    return combinations * prob_win * prob_lose
 
 
-print(combination(6, 4))
+print(binomial_distribution(5, 0.5, 0))
