9bfbf9381f07ab052b1aac431e30611a31247acb
Mgo <mgotm13@gmail.com>
2021-01-01 21:22:32 +0400

    Possion Distribution + code refactor

--- poisson_distribution.py
+++ poisson_distribution.py
@@ -1,18 +1,28 @@
-# poisson distribution probability mass function p(i)
-def poisson_pmf() -> float:
-    return 1
+import math
+from utilities.math import factorial
+
+
+# poisson distribution probability mass function p(i) i->success
+def poisson_pmf(param_lambda: float, success: int) -> float:
+    e_n_lambda = math.exp(-param_lambda)
+    lambda_e_i = param_lambda ** success
+    return e_n_lambda * (lambda_e_i / factorial(success))
 
 
 # poisson cumulative distribution function P(x<=i)
-def poisson_cdf() -> float:
+def poisson_cdf(param_lambda: float,  success: int = 1) -> float:
     return 1
 
 
 # poisson distribution expected value
-def poisson_expected() -> float:
+def poisson_expected(param_lambda: float,  success: int = 1) -> float:
     return 1
 
 
 # poisson distribution variance
-def poisson_variance() -> float:
+def poisson_variance(param_lambda: float,  success: int = 1) -> float:
+    return 1
+
+# print all data related to specific poisson distribution
+def poisson_all(param_lambda: float, success: int = 1,cumulative_i: int = 1):
     return 1
---
+++ utilities/util.py
@@ -0,0 +1,1 @@
+# todo add print all to print the data of all distribution
