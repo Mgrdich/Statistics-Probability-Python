3d019dc23a94d8827990bd8e7873b98f103f1fa5
Mgo <mgotm13@gmail.com>
2021-01-03 13:27:14 +0400

    hyper geometric + negative binomial distribution initial Commit

---
+++ distributions/hypergeometric_distribution.py
@@ -0,0 +1,18 @@
+# geometric probability mass function P(x=i)
+def hyper_geometric_pmf(probability: float, i: int):
+    return i
+
+
+# geometric cumulative distribution function P(x<=i)
+def hyper_geometric_cdf(probability: float, i: int):
+    return i
+
+
+# geometric distribution expected value
+def hyper_geometric_expected() -> float:
+    return 1
+
+
+# geometric distribution variance
+def hyper_geometric_variance() -> float:
+    return 1
---
+++ distributions/negative_binomial_distribution.py
@@ -0,0 +1,18 @@
+# geometric probability mass function P(x=i)
+def negative_binomial_pmf(probability: float, i: int):
+    return i
+
+
+# geometric cumulative distribution function P(x<=i)
+def negative_binomial_cdf(probability: float, i: int):
+    return i
+
+
+# geometric distribution expected value
+def negative_binomial__expected() -> float:
+    return 1
+
+
+# geometric distribution variance
+def negative_binomial_variance() -> float:
+    return 1
