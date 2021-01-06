09e7cfa8ef0bbd7991a5b6963b1905e72c11c232
Mgo <mgotm13@gmail.com>
2021-01-06 21:51:45 +0400

    decorators for validation

---
+++ utilities/decorators.py
@@ -0,0 +1,18 @@
+def binomial_validation():
+    return 1
+
+
+def geometric_validation():
+    return 1
+
+
+def hyper_geometric_validation():
+    return 1
+
+
+def negative_binomial_validation():
+    return 1
+
+
+def poisson_validation():
+    return 1
